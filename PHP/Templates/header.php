5a81b3c644bddc6c8e2959f2cb2a6477b1609c10
laura cano <109438420+lauracanovc@users.noreply.github.com>
2022-09-03 22:21:49 -0300

    Add files via upload

--- PHP/Templates/header.php
+++ PHP/Templates/header.php
@@ -8,15 +8,16 @@
     <link href='https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css' rel='stylesheet'>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="../../CSS/style.css" type=" text/css" rel='stylesheet'>
+    <link href="../../Resources/imgs/favicon.png" type="image/png" rel="icon">
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"></script>
-    <title>Entretaniment View</title>
+    <title>Real ReView</title>
 </head>
 <body>
 
 <nav class="navbar navbar-expand-sm" id="nav">
         <div class="container-fluid">
-            <img class="navbar-brand" src="" alt="Logo">
+            <img class="navbar-brand" src="../../Resources/imgs/logo_transparent.png" alt="Logo">
 
             <div class="collapse navbar-collapse" id="navbarNavDropdown">
                 <ul class="navbar-nav">
@@ -31,7 +32,7 @@
                         </li>
             </div>  <!--FORMULARIO DE INGRESO-->
                     <li class="nav-item d-flex dropdown">
-                    <a class="dropdown-toggle" data-bs-toggle="dropdown" data-bs-display="static" aria-expanded="false">Iniciar Sesión</a>
+                    <a class="nav-link" data-bs-toggle="dropdown" data-bs-display="static" aria-expanded="false">Iniciar Sesión</a>
                         <ul class="dropdown-menu dropdown-menu-lg-end">
                             <li>
                                 <form class="dropdown-item form-container" action="../Private/loginAuth.php" method="POST">
@@ -40,28 +41,25 @@
                                         <div class="col-sm-6">
                                             <div class="mb-3">
                                                 <label for="email">Email o Nombre de usuario</label><br>
-                                                <input type="text" name="user" id="email" placeholder="ingrese el Email" required><br>
+                                                <input type="text" name="user" id="email" placeholder="ingrese el Email"><br>
 
                                                 <label for="psw">Contraseña</label><br>
-                                                <input type="password" name="psw" id="psw" placeholder="ingrese la contraseña" required>
+                                                <input type="password" name="psw" id="psw" placeholder="ingrese la contraseña">
                                             </div>
                                                 <div class="formBtn">
                                                     <button type="submit" class="btn success">Ingresar</button>
                                                     <button type="button" class="btn cancel" onclick="limpiarForm();" >Cancelar</button>
                                                 </div>
                                         </div>
-                                        </form>
                                         <div class="col-sm-6 rigth text-center">
                                             <div class="mt-5">
-                                                <form action="../Private/loginAuth.php" method="POST">
-                                                    <input type="hidden" name="anon" value="1">
-                                                    <input type="submit" class="btn User" value="Ingresar Anonimamente">
-                                                </form>
+                                                <input type="submit" name="anon" class="btn User" value="Ingresar Anonimamente">
                                                 <p class="text-center">ó<p>
                                                 <button type="button" class="btn signin" onclick="openModal();">Registrarse</button>
                                             </div>
                                         </div>
                                     </div>
+                                </form>
                             </li>
                         </ul>
                     </li>
@@ -73,20 +71,20 @@
     <!--FORMULARIO DE REGISTRO-->
     <div class="modal">
         <div class="modal-content">
-            <form action="../Private/registrar.php" method="POST" class="form-container">
+            <form action="#" class="form-container">
                 <h1>Registrarse</h1>
 
                 <label for="nombre">Nombre de usuario</label>
-                <input type="text" name="nombre" placeholder="ingrese el nombre de usuario" required>
+                <input type="text" name="nombre" placeholder="ingrese el nombre de usuario">
 
                 <label for="email">Email</label>
-                <input type="email" name="email" placeholder="ingrese el Email" required>
+                <input type="email" name="email" placeholder="ingrese el Email">
                 <label for="psw">Contraseña</label>
-                <input type="password" name="psw" placeholder="ingrese la contraseña" required>
+                <input type="password" name="psw" placeholder="ingrese la contraseña">
 
                 <div class="formBtn">
-                <button type="submit" class="btn success">Registrarse</button>
-                <button type="button" class="btn cancel" onclick="closeModal();">Cancelar</button>
+                    <button type="submit" class="btn success">Registrarse</button>
+                    <button type="button" class="btn cancel" onclick="closeModal();">Cancelar</button>
                 </div>
             </form>
         </div>
