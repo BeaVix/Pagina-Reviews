577ba9de46677b1907817948d92ab5d2201b5611
laura cano <109438420+lauracanovc@users.noreply.github.com>
2022-09-06 20:21:07 -0300

    Add files via upload

---
+++ PHP/Public/peliculas.php
@@ -0,0 +1,21 @@
+<?php
+include '../Templates/header.php';
+?>
+
+<div class="container text-center">
+  <div class="row">
+    <div class="col-3 p-0">
+    <img src="../../Resources/imgs/logo.png" class="Poster" alt="Pelicula">
+    </div>
+    <div class="col-sm pt-5">
+      <div class="text-md-start">
+        <h2>Titulo de la Pelicula</h2>
+        <p>Descripcion de la pelicula</p>
+      </div>
+      <div class="d-flex">
+        <div>stars rating</div>
+        <div class="ms-auto"><i class='bx bx-comment'></i>Cant reviews</div>
+      </div>
+    </div>
+  </div>
+</div>
