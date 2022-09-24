cfdf2506e7020c081e0ce512fd1f9b005511e20b
laura cano <109438420+lauracanovc@users.noreply.github.com>
2022-09-24 20:01:50 -0300

    Add files via upload

--- PHP/Templates/listArts.php
+++ PHP/Templates/listArts.php
@@ -22,13 +22,13 @@
     $getRevs = $modo ? $revs->getReviewsLibros($row['ID']) : $revs->getReviewsPelis($row['ID']);
     $comms = $getRevs->fetchAll();
     ?>
-    <div class="row pb-4">
+    <div class="row text-start pb-4">
 
-    <div class="col-3 p-0">
+    <div class="col-md-auto">
       <img src="../../Resources/imgs/<?php echo $modo ? 'Libros/' : 'Peliculas/'; echo $row['Portada'];?>" class="Poster" alt="<?php echo $modo ? 'Libro' : 'Pelicula';?>">
     </div>
 
-    <div class="col-sm ps-5">
+    <div class="col-8 ps-auto">
       <div class="text-md-start">
         <h2><?php echo $row['Titulo'];?></h2>
         <h6><b><?php echo $row['Nombre'];?></b></h6>
@@ -70,7 +70,7 @@
               <p class="text-start text-post"><small><?php echo $values['Comentario'];?></small></p>
               <div class="card-body">
                 <div class="border-top d-flex">
-                  <button class="btn comment text-start" type="button" data-bs-toggle="collapse" data-bs-target="#Userdemo" aria-expanded="false" aria-controls="demo">
+                  <button class="btn comment text-start" type="button" data-bs-toggle="collapse" data-bs-target="#Userdemo<?php echo $values['ID'];?>" aria-expanded="false" aria-controls="demo">
                       <i class='bx bx-comment' style='color: black'></i>CantComment
                   </button>
                   <div class="btn-group">
@@ -84,7 +84,7 @@
                 </div>
             </div>
 
-            <div class="collapse" id="Userdemo">
+            <div class="collapse" id="Userdemo<?php echo $values['ID'];?>">
           <div class="card-body">
             <form>
               <div class="row">
