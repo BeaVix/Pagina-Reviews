a460f2725eefb21c159f0280ec15983e19b25a30
laura cano <109438420+lauracanovc@users.noreply.github.com>
2022-09-26 19:47:39 -0300

    Update AJAX.js

--- JS/AJAX.js
+++ JS/AJAX.js
@@ -1,12 +1,23 @@
 /* Iniciar sesión sin ir directamente a loginAuth.php usando Fetch */
 async function signIn(){
-    const name = document.querySelector('#email').value;
-    const pass = document.querySelector('#psw').value;
+    const form = document.querySelector('#form-iniciar-sesion');
 
+    let data = new FormData(form);
+
+    let response = await fetch('../Private/loginAuth.php', {
+        method: 'POST',
+	@@ -15,6 +12,40 @@ async function signIn(){
+    if(result == '1'){
+        reloadNav();
+    }else{
+        console.log(result);
+    }
+}
+
+/* Iniciar sesión anonimamente */
+async function anonSignIn(){
     let data = new FormData();
-    data.append('user', name);
-    data.append('psw', pass);
-
+    data.append('anon', true);
     let response = await fetch('../Private/loginAuth.php', {
         method: 'POST',
         body: data
@@ -14,29 +25,39 @@ async function signIn(){
     let result = await response.text();
     if(result == '1'){
         reloadNav();
-    }else{
     }
 }
 
-async function reloadNav(){
-    const nav = document.querySelector('#nav');
+/* Registrarse */
+async function signUp(){
+    const form = document.querySelector('#form-registrarse');
+
+    let data = new FormData(form);
+    let response = await fetch('../Private/registrar.php', {
+        method: 'POST',
+        body: data
+    });
+    let result = await response.text();
+    if (result == '1') {
+        document.querySelector('#email').value = data.get('email');
+        document.querySelector('#psw').value = data.get('psw');
+        signIn();
+    }else{
+        console.log(result);
+    }
+}
+
+	@@ -23,7 +54,6 @@ async function reloadNav(){
     let response = await fetch('../Templates/navbar.php');
     let result = await response.text();
     nav.innerHTML = result;
-    console.log(result);
 }
 
 async function signOut(){
-    let response = await fetch('../Private/logOut.php');
-    let result = await response.text();
+	@@ -32,9 +62,4 @@ async function signOut(){
     if(result){
         reloadNav();
     }
-}
-
-
-function anonSignIn(){
-
 }
 /*
 async function reviewForm(){
