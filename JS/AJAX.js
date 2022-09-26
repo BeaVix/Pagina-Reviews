551fa9dde61ed0a678b09ac82f58eee3e513060c
laura cano <109438420+lauracanovc@users.noreply.github.com>
2022-09-26 19:38:36 -0300

    Add files via upload

--- JS/AJAX.js
+++ JS/AJAX.js
@@ -1,25 +1,12 @@
 /* Iniciar sesión sin ir directamente a loginAuth.php usando Fetch */
 async function signIn(){
-    const form = document.querySelector('#form-iniciar-sesion');
+    const name = document.querySelector('#email').value;
+    const pass = document.querySelector('#psw').value;
 
-    let data = new FormData(form);
-
-    let response = await fetch('../Private/loginAuth.php', {
-        method: 'POST',
-        body: data
-    });
-    let result = await response.text();
-    if(result == '1'){
-        reloadNav();
-    }else{
-        console.log(result);
-    }
-}
-
-/* Iniciar sesión anonimamente */
-async function anonSignIn(){
     let data = new FormData();
-    data.append('anon', true);
+    data.append('user', name);
+    data.append('psw', pass);
+
     let response = await fetch('../Private/loginAuth.php', {
         method: 'POST',
         body: data
@@ -27,25 +14,7 @@ async function anonSignIn(){
     let result = await response.text();
     if(result == '1'){
         reloadNav();
-    }
-}
-
-/* Registrarse */
-async function signUp(){
-    const form = document.querySelector('#form-registrarse');
-
-    let data = new FormData(form);
-    let response = await fetch('../Private/registrar.php', {
-        method: 'POST',
-        body: data
-    });
-    let result = await response.text();
-    if (result == '1') {
-        document.querySelector('#email').value = data.get('email');
-        document.querySelector('#psw').value = data.get('psw');
-        signIn();
     }else{
-        console.log(result);
     }
 }
 
@@ -54,6 +23,7 @@ async function reloadNav(){
     let response = await fetch('../Templates/navbar.php');
     let result = await response.text();
     nav.innerHTML = result;
+    console.log(result);
 }
 
 async function signOut(){
@@ -63,3 +33,30 @@ async function signOut(){
         reloadNav();
     }
 }
+
+
+function anonSignIn(){
+
+}
+/*
+async function reviewForm(){
+    const comment = document.querySelector('#comment').value;
+
+    let content = new FormData();
+    content.append('comment', comment);
+
+    let response = await fetch('../Private/reviewsAuth.php', {
+        method: 'POST',
+        body: data
+    });
+    let res = await response.text();
+    if(res == 1){
+    
+    }
+}
+
+async function loadComment(){
+    const userComment = document.querySelector('.card-commet');
+}
+
+*/
