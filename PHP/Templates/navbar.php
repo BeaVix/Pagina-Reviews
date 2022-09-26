18c6ebef98fbd945771501394543c43bfd8dc012
laura cano <109438420+lauracanovc@users.noreply.github.com>
2022-09-26 13:46:21 -0300

    Add files via upload

--- PHP/Templates/navbar.php
+++ PHP/Templates/navbar.php
@@ -7,68 +7,70 @@
     $loggedIn = $sess->loggedIn;
 ?>
 
+<nav class="navbar navbar-expand-lg">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#"><img src="../../Resources/imgs/logo.png" alt="Logo" width="150" height="150" class="d-inline-block align-text-top"></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link" aria-current="page" href="index.php">Inicio</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="../Public/peliculas.php">Peliculas</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="../Public/libros.php">Libros</a>
+                        </li>
+            </div>
+              <!--FORMULARIO DE INGRESO-->
+                    <?php
+                        if (!$loggedIn) {
 
-<div class="container-fluid">
-    <img class="navbar-brand" src="../../Resources/imgs/logo_transparent.png" alt="Logo">
+                    ?>
+                    <li class="nav-item d-flex me-1 dropdown">
+                    <a class="nav-link" data-bs-toggle="dropdown" data-bs-display="static" aria-expanded="false">Iniciar Sesión</a>
+                        <ul class="dropdown-menu dropdown-menu-lg-end">
+                            <li>
+                                <form class="dropdown-item form-container" action="../Private/loginAuth.php" method="POST">
+                                    <h1>Iniciar Sesión</h1>
+                                    <div class="row">
+                                        <div class="col-sm-6">
+                                            <div class="mb-3">
+                                                <label for="email">Email o Nombre de usuario</label><br>
+                                                <input type="text" name="user" id="email" placeholder="ingrese el Email"><br>
 
-    <div class="collapse navbar-collapse" id="navbarNavDropdown">
-        <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link" aria-current="page" href="index.php">Inicio</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="../Public/peliculas.php">Peliculas</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="../Public/libros.php">Libros</a>
-                </li>
-    </div>  <!--FORMULARIO DE INGRESO-->
-            <?php
-                if (!$loggedIn) {
-
-            ?>
-            <li class="nav-item d-flex dropdown">
-                <a class="nav-link" data-bs-toggle="dropdown" data-bs-display="static" aria-expanded="false">Iniciar Sesión</a>
-                <ul class="dropdown-menu dropdown-menu-lg-end">
-                    <li>
-                        <form class="dropdown-item form-container" action="../Private/loginAuth.php" method="POST">
-                            <h1>Iniciar Sesión</h1>
-                            <div class="row">
-                                <div class="col-sm-6">
-                                    <div class="mb-3">
-                                        <label for="email">Email o Nombre de usuario</label><br>
-                                        <input type="text" name="user" id="email" placeholder="ingrese el Email"><br>
-
-                                        <label for="psw">Contraseña</label><br>
-                                        <input type="password" name="psw" id="psw" placeholder="ingrese la contraseña">
-                                    </div>
-                                        <div class="formBtn">
-                                            <button type="button" class="btn success" id="nav-login" onclick="signIn();">Ingresar</button>
-                                            <button type="button" class="btn cancel" onclick="limpiarForm();" >Cancelar</button>
+                                                <label for="psw">Contraseña</label><br>
+                                                <input type="password" name="psw" id="psw" placeholder="ingrese la contraseña">
+                                            </div>
+                                                <div class="formBtn">
+                                                    <button type="button" class="btn success" id="nav-login" onclick="signIn();">Ingresar</button>
+                                                    <button type="button" class="btn cancel" onclick="limpiarForm();" >Cancelar</button>
+                                                </div>
+                                                <span id="login-form-error" class="d-none">*Nombre de usuario y/o contraseña incorrectos</span>
                                         </div>
-                                        <span id="login-form-error" class="d-none">*Nombre de usuario y/o contraseña incorrectos</span>
-                                </div>
-                                <div class="col-sm-6 rigth text-center">
-                                    <div class="mt-5">
-                                        <button class="btn User" id="anon-btn">Ingresar Anonimamente</button>
-                                        <p class="text-center">ó<p>
-                                        <button type="button" class="btn signin" onclick="openModal();">Registrarse</button>
+                                        <div class="col-sm-6 rigth text-center">
+                                            <div class="mt-5">
+                                                <button class="btn User" id="anon-btn">Ingresar Anonimamente</button>
+                                                <p class="text-center">ó<p>
+                                                <button type="button" class="btn signin" onclick="openModal();">Registrarse</button>
+                                            </div>
+                                        </div>
                                     </div>
-                                </div>
-                            </div>
-                        </form>
+                                </form>
+                            </li>
+                        </ul>
                     </li>
+                    <?php 
+                    }else{
+                        include 'userProfileBtn.php';
+                    } 
+                    ?>
                 </ul>
-            </li>
-            <?php 
-            }else{
-                include 'userProfileBtn.php';
-            } 
-            ?>
-        </ul>
-</div>
-<a href="javascript:void(0);" class="icon" onclick="menuResponsive();"><i class='bx bx-menu'></i></a>
-
+    </div>
+</nav>
 <!--FORMULARIO DE REGISTRO-->
 <div class="modal">
     <div class="modal-content">
