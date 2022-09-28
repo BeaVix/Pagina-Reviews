a694fce98b535541a1481d6d8a867e769c56a21b
BeaVix <noEmail>
2022-09-27 21:05:25 -0300

    CAMBIO FINAL EN SERIO

--- PHP/Public/libros.php
+++ PHP/Public/libros.php
@@ -1,5 +1,5 @@
 <?php
   include '../Templates/header.php';
-  $modo = true; //El modo con el que se carga el template listArts.php
+  $modo = 1; //El modo con el que se carga el template listArts.php
   include '../Templates/listArts.php'
 ?>
