5cddcbe05202f8e1dad9d7cc5900aabb9bba6f58
BeaVix <noEmail>
2022-09-29 15:21:06 -0300

    Arreglado sql getUsersReview

--- PHP/Private/Clases/reviewsDB.php
+++ PHP/Private/Clases/reviewsDB.php
@@ -18,8 +18,13 @@
         }
         
         public function getUserReview($id){
-            $sql = "SELECT *, usuarios.Nombre FROM reviews INNER JOIN usuarios 
-                    ON reviews.Usuario_ID = usuarios.ID WHERE Usuario_ID = :id";
+            $sql = "SELECT reviews.Comentario, reviews.Cant_Estrellas, reviews.Likes, 
+                    reviews.Dislikes, reviews.Date, peliculas.Titulo AS pelicula_titulo, 
+                    peliculas.Portada AS pelicula_portada, libros.Portada AS libro_portada, 
+                    libros.Titulo AS libro_titulo FROM reviews INNER JOIN usuarios 
+                    ON reviews.Usuario_ID = usuarios.ID LEFT JOIN peliculas 
+                    ON reviews.Pelicula_ID = peliculas.ID LEFT JOIN libros 
+                    ON reviews.Libro_ID = libros.ID WHERE Usuario_ID = :id ORDER BY reviews.Date DESC";
             $stmt = $this->BDDCon->prepare($sql);
             $stmt->execute(array(':id' => $id));
 
