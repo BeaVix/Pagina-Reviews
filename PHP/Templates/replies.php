1859bb480964fb57bcd336f83fc0f45d6ea6beff
BeaVix <noEmail>
2022-09-29 20:56:09 -0300

    replies

--- PHP/Templates/replies.php
+++ PHP/Templates/replies.php
@@ -11,7 +11,7 @@
 ?>
 <div class="card-body border-top">
     <div class="d-flex">
-        <img src="<?php echo isset($r['Avatar']) ? "../../Uploads/".$nom.'/'.$avatar : "../../Resources/imgs/default_avatar.png"?>" class="card-img-user" alt="...">
+        <img src="<?php echo isset($r['Avatar']) ? "../../Uploads/".$r['Nombre'].'/'.$r['Avatar'] : "../../Resources/imgs/default_avatar.png"?>" class="card-img-user" alt="...">
         <h5 class="card-p m-2"><?php echo $r['Nombre']; ?></b>
     </div>
     <p class="text-start text-post"><small><?php echo $r['Comentario']; ?></small></p>
