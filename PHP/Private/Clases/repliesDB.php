4bc0f43341aed1ef9dfab90676b07f0ac7938631
laura cano <109438420+lauracanovc@users.noreply.github.com>
2022-10-01 12:58:50 -0300

    Add files via upload

--- PHP/Private/Clases/repliesDB.php
+++ PHP/Private/Clases/repliesDB.php
@@ -1,23 +1,26 @@
 <?php
+
     namespace BDD\Tables;
 
     include_once 'database.php';
 
     use BDD\Database;
 
-    class Replies extends Database{
-        /* Consigue las respuestas de una review por su id */
-        public function getRepliesReview($id){
-            $sql = 'SELECT replies.Comentario, replies.Date, replies.Likes, 
-                    replies.Dislikes, usuarios.Nombre, usuarios.Avatar FROM 
-                    replies INNER JOIN usuarios ON replies.Usuario_ID = 
-                    usuarios.ID WHERE Review_ID = :id';
+    class Replies extends Database {
 
+        public function addReplies(array $datos){
+            $stmt = $this->BDDCon->prepare("INSERT INTO replies (Usuario_ID, Review_ID, Comentario, Date, Likes, Dislikes) VALUES (?, ?, ?, ?, 0, 0)");
+            $stmt->execute($datos);
+            return $stmt;
+        }
+
+        public function getReplies(){
+            $sql = "SELECT *, usuarios.Nombre FROM replies INNER JOIN usuarios ON replies.Usuario_ID = usuarios.ID ORDER BY replies.Date DESC";
             $stmt = $this->BDDCon->prepare($sql);
-            $stmt->execute(array(':id' => $id));
+            $stmt->execute();
 
             return $stmt;
         }
     }
-    
+
 ?>
