bc2b30bcfac986cc45a1b43e7f7369d0af715f39
BeaVix <noEmail>
2022-10-03 16:27:20 -0300

    Funcion que carga las estrellas arreglado

--- PHP/Private/starLoad.php
+++ PHP/Private/starLoad.php
@@ -1,15 +1,15 @@
 <?php
 function starLoad($num){
-    $string = "";
-    $half = "";
-    if (is_float($num)){
-      $num = floor($num);
-      $half .= "<i class=\"bx bxs-star-half\"></i>";
-    }
-    $string .= str_repeat("<i class=\"bx bxs-star\"></i>", $num);
-    $string .= $half;
-    $string .= str_repeat("<i class=\"bx bx-star\"></i>", (5-$num));
-
-    return $string;
+  $string = "";
+  $half = "";
+  $string .= str_repeat("<i class=\"bx bxs-star\"></i>", $num);
+  if (strstr($num, '.')){
+    $num = ceil($num);
+    $half .= "<i class=\"bx bxs-star-half\"></i>";
   }
+  $string .= $half;
+  $string .= str_repeat("<i class=\"bx bx-star\"></i>", (5-$num));
+
+  return $string;
+}
 ?>
